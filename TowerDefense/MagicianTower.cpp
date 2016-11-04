25a05514ca5d41222f8f9a874ea6c9a0c324e467
DaniHdez <dannyha07@gmail.com>
2016-11-04 04:07:16 -0600

    img sounds

--- TowerDefense/MagicianTower.cpp
+++ TowerDefense/MagicianTower.cpp
@@ -1,4 +1,7 @@
 #include "MagicianTower.h"
+#include "Bullet.h"
+#include "Game.h"
+#include <QTimer>
 
 extern Game *game;
 
@@ -9,35 +12,24 @@ MagicianTower::MagicianTower(QGraphicsItem *parent){
 }
 
 void MagicianTower::fire(){
-    Bullet *bullet1 = new Bullet();
-    Bullet *bullet2 = new Bullet();
-    Bullet *bullet3 = new Bullet();
 
     QMediaPlayer *magicsound = new QMediaPlayer();
-    magicsound->setMedia(QUrl(""));
+    magicsound->setMedia(QUrl("qrc:/sounds/MagicSound.mp3"));
     magicsound->setVolume(50);
     magicsound->play();
 
-    QPixmap fired(":/image/blueFire.png");
+    QPixmap fired(":/images/blueFire.png");
     QPixmap resizeBall = fired.scaled(QSize(40,40), Qt::KeepAspectRatio);
-    bullet1->setPixmap(QPixmap(resizeBall));
-    bullet2->setPixmap(QPixmap(resizeBall));
-    bullet3->setPixmap(QPixmap(resizeBall));
+    Bullet * bullet = new Bullet();
+    bullet->setPixmap(QPixmap(resizeBall));
+    bullet->setPos(x()+44,y()+44);
 
-    bullet1->setPos(x()+44,y()+44);
-    bullet2->setPos(x()+44,y()+44);
-    bullet3->setPos(x()+44,y()+44);
 
     QLineF ln(QPointF(x()+44,y()+44),attack_dest);
     int angle=-1 *ln.angle();
 
-    bullet1->setRotation(angle);
-    bullet2->setRotation(angle+10);
-    bullet3->setRotation(angle-10);
-
-    game->scene->addItem(bullet1);
-     game->scene->addItem(bullet2);
-      game->scene->addItem(bullet3);
+    bullet->setRotation(angle);
+    game->scene->addItem(bullet);
 }
 
 
